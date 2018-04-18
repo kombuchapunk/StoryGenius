f9ba695479fc6a1e63c08fa6d91a973f235be438
Chase Bowden and Misha Petrov <Guest@Epicodus-3A.local>
2018-04-18 15:13:14 -0700

    add functionality to add and delete random stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'image_searcher'
 
 class StoriesController < ApplicationController
   def index
@@ -14,7 +15,30 @@ class StoriesController < ApplicationController
 
   def new
     @story = Story.new
-    @random_word = Faker::Hipster.word
+    random_word = Faker::Hipster.unique.word
+    random_img = ImageSearcher::Client.new.search(query: random_word, count: 1)
+    @random_img = random_img[0]["url"]
     render :new
   end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to  stories_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to stories_path
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:name, :imgLink)
+  end
+
 end
