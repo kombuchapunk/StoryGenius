9f8b42267b4403b476a8ccebc9e19f343bb0eda9
Chase Bowden and Misha Petrov <chase+misha@>
2018-04-19 16:23:24 -0700

    add grid of cards for stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,17 @@ class Story < ActiveRecord::Base
   has_many :sentences
   validates :name, :presence => true
   validates :imgLink, :presence => true
+
+  def getPreview
+    preview = ''
+    letter_count = 0
+    self.sentences.each do |sentence|
+      preview = preview + sentence.words.capitalize + ". " 
+    end
+    if (preview.length > 150)
+      preview = preview[0..150].gsub(/\s\w+\s*$/,'...')
+    end
+    return preview
+  end
+
 end
